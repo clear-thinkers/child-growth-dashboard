80ce6796e94004261ba9f93f5122a591826853ac
clear-thinkers <clearthinkers1984@gmail.com>
2025-03-05 16:48:28 -0500

    Update vite.config to create build repository

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/child-growth-dashboard/', // Make sure this matches the repository name
+  base: '/child-growth-dashboard/',
   plugins: [react()],
+  build: {
+    outDir: 'build', // Ensure build output is placed in the 'build' directory
+  },
 });
